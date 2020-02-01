daf8366817ed709194017d010a9be060b628fc62
Fiorella Quino O <dkafio@gmail.com>
2020-02-01 16:47:05 -0500

    :art: add warning box for incorrect users

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .forms import LoginForm
+import requests as req
 
 
 def login(request):
     context = {}
+    context['show_alert'] = False
 
     if request.method == 'GET':
         form = LoginForm()
@@ -11,7 +13,21 @@ def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            parameters = form.cleaned_data
+            payload = {"driver": parameters}
+            print(payload)
+            try:
+                response = req.post(
+                    'https://jettymx-st.herokuapp.com/api/drivers/session', json=payload)
+
+                response_parameters = response.json()
+                if "code" in response.json():
+                    context['show_alert'] = True
+                    context['message'] = response_parameters['message']
+                print(response_parameters)
+
+            except Exception as e:
+                print(e)
 
     context['form'] = form
     return render(request, "main/login.html", context)
