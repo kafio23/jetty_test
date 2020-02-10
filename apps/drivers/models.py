4905a3244db44b33e8ba5cbb33b73b4072a66434
Fiorella Quino O <dkafio@gmail.com>
2020-02-10 14:49:53 -0500

    :bug: fix migrations

--- apps/drivers/models.py
+++ apps/drivers/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Driver(models.Model):
-    driver_id = models.IntegerField(null=False, unique=True)
+    driver_id = models.IntegerField()
     email = models.CharField(max_length=30, null=False, unique=True)
     auth_token = models.CharField(max_length=50, null=False)
