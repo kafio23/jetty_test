d31ef28c1f01dea91cf163e95e7f0a7c349bda95
Fiorella Quino O <dkafio@gmail.com>
2020-02-10 18:53:47 -0500

    :art: trips front first steps

--- apps/drivers/views.py
+++ apps/drivers/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Driver
 import requests as req
 
 
 def driver(request, driver_id):
     context = {}
-    drivers = Driver.objects.get(driver_id=driver_id)
-    print(drivers.email)
-    print(driver_id)
+    context['trips'] = []
+
+    driver = Driver.objects.get(driver_id=driver_id)
+
+    authorization = 'Token {},email={}'.format(
+        driver.auth_token, driver.email)
+    headers = {'Content-Type': 'application/json',
+               'Authorization': authorization}
+    try:
+        response = req.get(
+            'https://jettymx-st.herokuapp.com/api/drivers/trips', headers=headers)
+
+        if response.status_code == 200:
+            trips = response.json()
+            context['trips'] = trips
+        else:
+            return redirect('login_url')
+    except:
+        return redirect('login_url')
 
     return render(request, "drivers/trips_list.html", context)
